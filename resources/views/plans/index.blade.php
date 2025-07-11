d1f6acac07fb2b5be716286fc1a70b499056e9fa
Mohammad Rasoul Abdolmaleki <128932603+mDevMaleki@users.noreply.github.com>
2025-07-11 12:25:58 +0330

    Translate plan interface to Persian

--- resources/views/plans/index.blade.php
+++ resources/views/plans/index.blade.php
@@ -6,10 +6,10 @@
         <!-- BEGIN PAGE HEADING -->
         <div class="page-head bg-grey-100 padding-top-15 no-padding-bottom">
             @include('flash::message')
-            <h1 class="page-title no-line-height">Plans
+            <h1 class="page-title no-line-height">برنامه‌ها
                 @permission(['manage-gymie','manage-plans','add-plan'])
-                <a href="{{ action('PlansController@create') }}" class="page-head-btn btn-sm btn-primary active" role="button">Add New</a>
-                <small>Details of all gym plans</small>
+                <a href="{{ action('PlansController@create') }}" class="page-head-btn btn-sm btn-primary active" role="button">افزودن برنامه</a>
+                <small>جزئیات تمام برنامه‌های باشگاه</small>
             </h1>
             @permission(['manage-gymie','pagehead-stats'])
             <h1 class="font-size-30 text-right color-blue-grey-600 animated fadeInDown total-count pull-right">
@@ -17,7 +17,7 @@
                       data-from="0" data-to="{{ $count }}"
                       data-speed="600"
                       data-refresh-interval="10"></span>
-                <small class="color-blue-grey-600 display-block margin-top-5 font-size-14">Total Plans</small>
+                <small class="color-blue-grey-600 display-block margin-top-5 font-size-14">تعداد کل برنامه‌ها</small>
             </h1>
             @endpermission
             @endpermission
@@ -33,7 +33,7 @@
                                 <div class="col-xs-12 col-md-3 pull-right">
                                     {!! Form::Open(['method' => 'GET']) !!}
                                     <div class="btn-inline pull-right">
-                                        <input name="search" id="search" type="text" class="form-control padding-right-35" placeholder="Search...">
+                                        <input name="search" id="search" type="text" class="form-control padding-right-35" placeholder="جستجو...">
                                         <button class="btn btn-link no-shadow bg-transparent no-padding-top padding-right-10" type="button">
                                             <i class="ion-search"></i></button>
                                     </div>
@@ -43,20 +43,20 @@
                             </div>
 
                             @if($plans->count() == 0)
-                                <h4 class="text-center padding-top-15">Sorry! No records found</h4>
+                                <h4 class="text-center padding-top-15">متاسفانه موردی یافت نشد</h4>
                             @else
 
                                 <table id="plans" class="table table-bordered table-striped">
                                     <thead>
                                     <tr>
-                                        <th>Plan Code</th>
-                                        <th>Plan Name</th>
-                                        <th>Service Name</th>
-                                        <th>Plan Details</th>
-                                        <th>Days</th>
-                                        <th>Amount</th>
-                                        <th>Status</th>
-                                        <th class="text-center">Actions</th>
+                                        <th>کد برنامه</th>
+                                        <th>نام برنامه</th>
+                                        <th>نام خدمت</th>
+                                        <th>جزئیات برنامه</th>
+                                        <th>روز</th>
+                                        <th>مبلغ</th>
+                                        <th>وضعیت</th>
+                                        <th class="text-center">عملیات</th>
                                     </tr>
                                     </thead>
                                     <tbody>
@@ -74,7 +74,7 @@
 
                                             <td class="text-center">
                                                 <div class="btn-group">
-                                                    <button type="button" class="btn btn-info">Actions</button>
+                                                    <button type="button" class="btn btn-info">عملیات</button>
                                                     <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                                                         <span class="caret"></span>
                                                         <span class="sr-only">Toggle Dropdown</span>
@@ -83,7 +83,7 @@
                                                         <li>
                                                             @permission(['manage-gymie','manage-plans','edit-plan'])
                                                             <a href="{{ action('PlansController@edit',['id' => $plan->id]) }}">
-                                                                Edit details
+                                                                ویرایش جزئیات
                                                             </a>
                                                             @endpermission
                                                         </li>
@@ -95,10 +95,10 @@
                                                             <a href="#"
                                                                class="delete-record"
                                                                data-dependency="{{ $dependency }}"
-                                                               data-dependency-message="You have members assigned to this plan, either delete them or assign them to new plan"
+                                                               data-dependency-message="برای این برنامه کاربرانی ثبت شده‌اند، ابتدا آن‌ها را حذف یا به برنامه دیگری منتقل کنید"
                                                                data-delete-url="{{ url('plans/'.$plan->id.'/archive') }}"
                                                                data-record-id="{{$plan->id}}">
-                                                                Delete plan
+                                                                حذف برنامه
                                                             </a>
                                                             @endpermission
                                                         </li>
@@ -116,7 +116,7 @@
                                 <div class="row">
                                     <div class="col-xs-6">
                                         <div class="gymie_paging_info">
-                                            Showing page {{ $plans->currentPage() }} of {{ $plans->lastPage() }}
+                                            نمایش صفحه {{ $plans->currentPage() }} از {{ $plans->lastPage() }}
                                         </div>
                                     </div>
 
